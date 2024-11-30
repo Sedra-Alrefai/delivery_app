bfc873e11e9ad468921bee33d571cdf30dc81cb0
maram <maramalm367@gmail.com>
2024-11-30 23:20:54 +0300

    add api classes

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,13 @@
+class EndPoint {
+  static String baseUrl = "";
+  static String signIn = "";
+}
+
+class ApiKey {
+  static String status = "status";
+  static String errorMessage = "ErrorMessage";
+  static String email = "email";
+  static String password = "password";
+  static String token = "token";
+  static String id = "id";
+}
